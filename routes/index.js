1a8847d851d8b8b6c43d02385ffaf5e4c7110cc8
Abdellah <abdellahmoiuda@gmail.com>
2024-03-09 18:27:09 +0100

    cheak Linking to Public Server

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const { models, model } = require("mongoose");
 let router = exprss.Router();
 
 router.get("/", (req, res) => {
-  res.send("Connected");
+  res.send("Hello, world! it's connected");
 });
 
 module.exports = router;
