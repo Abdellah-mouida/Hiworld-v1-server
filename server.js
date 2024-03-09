b3e03a1bf7a75ff7c0aa8232e99d63cfd6724061
Abdellah <abdellahmoiuda@gmail.com>
2024-03-09 18:43:09 +0100

    Config Server with RENDETR

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(bodyParser.urlencoded({ extended: true, limit: "15mb" }));
 app.use(express.json({ limit: "15mb" }));
 app.use(
   cors({
-    origin: "https://hiworld-abdellah.netlify.app",
+    origin: "https://hiworld-client.onrender.com",
   })
 );
 // Enable CORS
