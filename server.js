f8956a8fcf5e16fd0e2b7295eaba5192995a6f95
Abdellah <abdellahmoiuda@gmail.com>
2024-03-10 10:51:09 +0000

    Allowing all Routes To get data

--- server.js
+++ server.js
@@ -25,11 +25,7 @@ db.once("open", () => console.log("Connected To DataBase"));
 app.use(bodyParser.urlencoded({ extended: true, limit: "15mb" }));
 // app.use(bodyParser.json({ limit: "15mb" }));
 app.use(express.json({ limit: "15mb" }));
-app.use(
-  cors({
-    origin: "https://hiworld-client.netlify.app",
-  })
-);
+app.use(cors());
 // Enable CORS
 
 app.use("/", indexRouter);
